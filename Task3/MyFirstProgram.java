add0317862e7e26ffd9acd8f2ee2ee6e9d5e5021
Japan-Manul <136632309+Japan-Manul@users.noreply.github.com>
2025-09-22 19:32:38 +0300

    Task 3 initial commit
    Add MySecondClass with basic arithmetic logic and update MyFirstClass main()

---
+++ Task3/MyFirstProgram.java
@@ -0,0 +1,46 @@
+class MyFirstClass {
+ 	public static void main(String[] s) {
+		MySecondClass o = new MySecondClass(5, 2);
+		System.out.println(o.remainder());
+		for (int i = 1; i <= 8; i++) {
+ 			for (int j = 1; j <= 8; j++) {
+     				o.setA(i);
+     				o.setB(j);
+     				System.out.print(o.remainder());
+     				System.out.print(" ");
+ 			}
+ 			System.out.println();
+		}
+
+ 	}
+}
+
+class MySecondClass {
+	private int a;
+	private int b;
+
+	public MySecondClass(int a, int b) {
+        	this.a = a;
+        	this.b = b;
+   	}
+
+    	public int getA() {
+        	return a;
+    	}
+
+    	public int getB() {
+        	return b;
+    	}
+
+    	public void setA(int a) {
+        	this.a = a;
+    	}
+
+    	public void setB(int b) {
+        	this.b = b;
+    	}
+
+	public int remainder() {
+        	return a % b;
+	}
+}
